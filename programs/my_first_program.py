e8d9ed1575a3aacde46fbe97e2c8051daf852cb7
Vijayaraj-Anbalagan <vijayaraj.anbalagan@gmail.com>
2024-06-25 16:40:11 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,19 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
+    
+    # Inputs from party1
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+    
+    # Conditional operations based on comparison of my_int1 and my_int2
+    if my_int1 > my_int2:
+        result = my_int1 * my_int2  # Product if my_int1 is greater
+    elif my_int1 < my_int2:
+        result = my_int2 - my_int1  # Difference if my_int2 is greater
+    else:
+        result = my_int1 * my_int1  # Square if they are equal
+    
+    # Return the result as an output to party1
+    return [Output(result, "conditional_output", party1)]
